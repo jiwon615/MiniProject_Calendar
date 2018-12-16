b9fb6b1710c1cfbce650d670fe97e561709520c0
jiwonjung <seg615@naver.com>
2018-12-16 22:20:17 +0900

    getting max days of the month

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -1,15 +1,24 @@
 package calendar;
 
+import java.util.Scanner;
+
+
 public class Calendar {
+	
+	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	public int getMaxDaysOfMonth(int month) {   //정수로 달을 입력받아 해당 달의 최대 일수를 반환
+		return MAX_DAYS[month - 1];  //인덱스는 0부터 시작하므로 1을 빼줘야 함 
+	}
 
+	
 	public static void main(String[] args) {
-
-		System.out.println(" 월 화 수 목 금 토 일");
-		System.out.println("--------------------");
-		System.out.println(" 1  2  3  4  5  6  7");
-		System.out.println(" 8  9 10 11 12 13 14");
-		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28");
+		Scanner scanner = new Scanner(System.in);
+		Calendar cal = new Calendar(); //클래스에 대한 객체 생성 
+		System.out.println("달을 입력하세요.");
+		int month = scanner.nextInt();  //입력받는 달은 정수형 
+		System.out.println(month + "월은 " + cal.getMaxDaysOfMonth(month) + "일까지 있습니다.");  //클래스의 함수 사용하므로 cal.getMaxDaysOfMonth(month)
+		
+		scanner.close();
 
 
 	}
